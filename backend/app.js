3797a87102cb8b4d93ae2c1d307591c3cc3dbe6d
jannathsyeda <jannathsyeda7@gmail.com>
2022-04-10 20:15:55 +0600

    complete

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-.connect('mongodb+srv://jannath:VrL6yaKs8wHQ5anO@cluster0.ku9hx.mongodb.net/Places?retryWrites=true&w=majority')
+.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ku9hx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 
   .then(() => {
     app.listen(5000);
